1c8c25153e02d0492ce6d77811daaa895908b1e2
ArthurYang <64062290+c-arthur-yang@users.noreply.github.com>
2023-11-03 14:12:05 +0800

    Update __init__.py

--- SEDR/__init__.py
+++ SEDR/__init__.py
@@ -1,7 +1,7 @@
 from .graph_func import graph_construction, combine_graph_dict
 from .utils_func import adata_preprocess, fix_seed
 from .SEDR_model import Sedr
-from .clustering_func import res_search_fixed_clus, mclust_R
+from .clustering_func import res_search_fixed_clus_leiden, mclust_R
 
 
 __all__ = [
@@ -10,6 +10,6 @@ __all__ = [
     "adata_preprocess",
     "fix_seed",
     "Sedr",
-    "res_search_fixed_clus",
+    "res_search_fixed_clus_leiden",
     "mclust_R"
 ]
